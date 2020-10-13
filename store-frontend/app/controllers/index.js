b24e3aa193b2b83ee000300eb0e107980c89c644
Andonee <mateusz.poprawa@gmail.com>
2020-10-13 19:36:37 +0200

    add local date products

---
+++ store-frontend/app/controllers/index.js
@@ -0,0 +1,8 @@
+import Controller from "@ember/controller";
+
+export default class IndexController extends Controller {
+  constructor(...args) {
+    super(...args);
+    console.log(this.model);
+  }
+}
---
+++ store-frontend/app/data/products.js
@@ -0,0 +1,15 @@
+export const products = [
+  {
+    title: "Shoes",
+    category: "men's fashion",
+    price: 39.99,
+    image: "https://images-na.ssl-images-amazon.com/images/I/41Leu3gBUFL.jpg",
+  },
+  {
+    title: "Televisor",
+    category: "electronics",
+    price: 238.0,
+    image:
+      "https://www.mediaexpert.pl/media/cache/gallery/product/3/374/717/549/fpqxeivv/images/24/2476126/Telewizor-SAMSUNG-LED-UE43TU8502-skos-2.jpg",
+  },
+];
